2b9da408034faae3b6348e3e1d0f72e70f44dcab
franksabogal04 <franksabogal04@gmail.com>
2023-05-19 17:47:52 -0400

    some extra code

--- server/index.js
+++ server/index.js
@@ -5,8 +5,9 @@ const { v1: uuidv4 } = require('uuid')
 const jwt = require('jsonwebtoken')
 const cors = require('cors')
 const bcrypt = require('bcrypt')
+require('dotenv').config()
 
-const uri = ''
+const uri = process.env.URL
 /* was unable to get the URI for line 4 from mongo, check slack for details */
 
 const app = express()
@@ -46,9 +47,11 @@ app.post('/signup', async (req, res) => {
             expiresIn: 60 * 24,
         })
 
-        res.status(201).json({ token, userId: generateduserId, email: sanitizedEmail})
+        res.status(201).json({ token, userId: generateduserId, /*email: sanitizedEmail*/})
     } catch (err) {
         console.log(err)
+    } finally{
+        await client.close()
     }
 })
 
@@ -85,8 +88,153 @@ app.get('/users', async (req, res) => {
         const database = client.db('app-data')
         const users = database.collection('users')
         
-        const returnedUsers = await users.find().toArray()
-        res.send(returnedUsers)
+        const query = {user_id: userId}
+        const user = await users.findOne(query)
+        res.send(user)
+        /*const returnedUsers = await users.find().toArray()
+        res.send(returnedUsers)*/
+    } finally {
+        await client.close()
+    }
+})
+
+// Update User with a match
+app.put('/addmatch', async (req, res) => {
+    const client = new MongoClient(uri)
+    const {userId, matchedUserId} = req.body
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const query = {user_id: userId}
+        const updateDocument = {
+            $push: {matches: {user_id: matchedUserId}}
+        }
+        const user = await users.updateOne(query, updateDocument)
+        res.send(user)
+    } finally {
+        await client.close()
+    }
+})
+
+// Get all Users by userIds in the Database
+app.get('/users', async (req, res) => {
+    const client = new MongoClient(uri)
+    const userIds = JSON.parse(req.query.userIds)
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const pipeline =
+            [
+                {
+                    '$match': {
+                        'user_id': {
+                            '$in': userIds
+                        }
+                    }
+                }
+            ]
+
+        const foundUsers = await users.aggregate(pipeline).toArray()
+
+        res.json(foundUsers)
+
+    } finally {
+        await client.close()
+    }
+})
+
+// Get all the Gendered Users in the Database
+app.get('/gendered-users', async (req, res) => {
+    const client = new MongoClient(uri)
+    const gender = req.query.gender
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+        const query = {gender_identity: {$eq: gender}}
+        const foundUsers = await users.find(query).toArray()
+        res.json(foundUsers)
+
+    } finally {
+        await client.close()
+    }
+})
+
+// Update a User in the Database
+app.put('/user', async (req, res) => {
+    const client = new MongoClient(uri)
+    const formData = req.body.formData
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const query = {user_id: formData.user_id}
+
+        const updateDocument = {
+            $set: {
+                first_name: formData.first_name,
+                dob_day: formData.dob_day,
+                dob_month: formData.dob_month,
+                dob_year: formData.dob_year,
+                show_gender: formData.show_gender,
+                gender_identity: formData.gender_identity,
+                gender_interest: formData.gender_interest,
+                url: formData.url,
+                about: formData.about,
+                matches: formData.matches
+            },
+        }
+
+        const insertedUser = await users.updateOne(query, updateDocument)
+
+        res.json(insertedUser)
+
+    } finally {
+        await client.close()
+    }
+})
+
+// Get Messages by from_userId and to_userId
+app.get('/messages', async (req, res) => {
+    const {userId, correspondingUserId} = req.query
+    const client = new MongoClient(uri)
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const messages = database.collection('messages')
+
+        const query = {
+            from_userId: userId, to_userId: correspondingUserId
+        }
+        const foundMessages = await messages.find(query).toArray()
+        res.send(foundMessages)
+    } finally {
+        await client.close()
+    }
+})
+
+// Add a Message to our Database
+app.post('/message', async (req, res) => {
+    const client = new MongoClient(uri)
+    const message = req.body.message
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const messages = database.collection('messages')
+
+        const insertedMessage = await messages.insertOne(message)
+        res.send(insertedMessage)
     } finally {
         await client.close()
     }
